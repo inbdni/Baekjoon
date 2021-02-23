677cba4464e02925af503d0c56950413739dd255
백지민 <jimin@baegjimin-ui-MacBookPro.local>
2021-02-23 17:49:12 +0900

    DP

--- 10422_괄호.cpp
+++ 10422_괄호.cpp
@@ -6,47 +6,31 @@ using namespace std;
 
 long long m[5001];
 
-// void init()
-// {
-// 	int i, j;
-
-// 	m[0] = 1;
-// 	for (i = 2; i <= 5000; i += 2)
-// 	{
-// 		for (j = 2; j <= i; j += 2)
-// 		{
-// 			m[i] += m[j - 2] * m[i - j];
-// 			m[i] %= 1000000007;
-// 		}
-// 	}
-// }
-
-long long dp(int L)
+void init()
 {
-	int i;
-	
-	if (m[L] != 0 || L % 2 == 1)
+	int i, j;
+
+	m[0] = 1;
+	for (i = 2; i <= 5000; i += 2)
 	{
-		return m[L];
+		for (j = 2; j <= i; j += 2)
+		{
+			m[i] += m[j - 2] * m[i - j];
+			m[i] %= MOD;
+		}
 	}
-	for (i = 2; i <= L; i += 2)
-	{
-		m[L] += dp(i - 2) * dp(L - i);
-		m[L] %= MOD;
-	}
-	return m[L];
 }
 
 int main()
 {
 	int t, T, L;
 
-	// init();
+	init();
 	cin >> T;
 	for (t = 0; t < T; t++)
 	{
 		cin >> L;
-		cout << dp(L) << endl;
+		cout << m[L] << endl;
 	}
 	return 0;
 }
